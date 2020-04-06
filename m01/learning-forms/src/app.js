b98e1bde78a6f39a10be3eaf5e733230111b96c9
André Lucas <andrelrs.v@outlook.com>
2020-04-06 16:45:05 -0300

    M1#A31

--- m01/learning-forms/src/app.js
+++ m01/learning-forms/src/app.js
@@ -6,8 +6,7 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: '2',
-      checked: false
+      value: 'valor para teste'
     }
   }
 
@@ -16,14 +15,7 @@ class App extends Component {
     return (
       <div>
         <form>
-          <select
-            value={this.state.value}
-            onChange={(e) => this.setState({ value: e.target.value })}
-          >
-            <option value='1'>Opção 1</option>
-            <option value='2'>Opção 2</option>
-            <option value='3'>Opção 3</option>
-          </select>
+          <textarea defaultValue={this.state.value} />
         </form>
       </div>
     )
