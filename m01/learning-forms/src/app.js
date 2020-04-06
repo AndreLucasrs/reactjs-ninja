4d527704075a45f9c9f85ac0817df881ca02213a
André Lucas <andrelrs.v@outlook.com>
2020-04-06 17:47:44 -0300

    M1#A33

--- m01/learning-forms/src/app.js
+++ m01/learning-forms/src/app.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: 'valor para teste'
+      checked: false,
+      showContent: false
     }
   }
 
@@ -14,20 +15,23 @@ class App extends Component {
   render () {
     return (
       <div>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            console.log('event', e)
-          }}
-          onChange={(e) => {
-            console.log('name ', e.target.name)
-            console.log('value ', e.target.value)
-          }}
-        >
-          <input type='text' name='name' />
-          <input type='email' name='email' />
-          <button type='submit'>Enviar</button>
-        </form>
+        <label>
+          <input
+            type='checkbox'
+            checked={this.state.checked}
+            onChange={() => {
+              this.setState({
+                checked: !this.state.checked
+              }, () => {
+                this.setState({
+                  showContent: this.state.checked
+                })
+              })
+            }}
+          />
+          Mostrar conteúdo
+        </label>
+        {this.state.showContent && <div>Olha eu aqui!</div>}
       </div>
     )
   }
