8b8f269d4dc331364e230c4a9743ff28d32eae92
André Lucas <andrelrs.v@outlook.com>
2020-04-08 01:07:45 -0300

    M1#A41 e desafio api git

--- m01/github-app/src/app.js
+++ m01/github-app/src/app.js
@@ -2,27 +2,49 @@
 
 import React, { Component } from 'react'
 import AppContent from './components/app-content'
+import ajax from '@fdaciuk/ajax'
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-      userinfo: {
-        username: 'André Lucas',
-        photo: 'https://avatars3.githubusercontent.com/u/12537847?v=4',
-        login: 'AndreLucasrs',
-        repos: 22,
-        followers: 7,
-        following: 40
-      },
-      repos: [{
-        name: 'Repo',
-        link: '#'
-      }],
-      starred: [{
-        name: 'Repo',
-        link: '#'
-      }]
+      userinfo: null,
+      repos: [],
+      starred: []
+    }
+  }
+
+  handleSearch (e) {
+    const URL_API = 'https://api.github.com/users'
+    const value = e.target.value
+    const keyCode = e.which || e.keyCode
+    const ENTER = 13
+    if (keyCode === ENTER) {
+      ajax().get(`${URL_API}/${value}`)
+        .then((result) => {
+          this.setState({
+            userinfo: {
+              username: result.name,
+              photo: result.avatar_url,
+              login: result.login,
+              repos: result.public_repos,
+              followers: result.followers,
+              following: result.following
+            }
+          })
+        })
+      ajax().get(`${URL_API}/${value}/repos`)
+        .then((result) => {
+          this.setState({
+            repos: result
+          })
+        })
+      ajax().get(`${URL_API}/${value}/starred`)
+        .then((result) => {
+          this.setState({
+            starred: result
+          })
+        })
     }
   }
 
@@ -32,6 +54,7 @@ class App extends Component {
         userinfo={this.state.userinfo}
         repos={this.state.repos}
         starred={this.state.starred}
+        handleSearch={(e) => this.handleSearch(e)}
       />
     )
   }
--- m01/github-app/src/components/app-content.js
+++ m01/github-app/src/components/app-content.js
@@ -6,9 +6,9 @@ import UserInfo from './user-info'
 import Actions from './actions'
 import Repos from './repos'
 
-const AppContent = ({ userinfo, repos, starred }) => (
+const AppContent = ({ userinfo, repos, starred, handleSearch }) => (
   <div className='app'>
-    <Search />
+    <Search handleSearch={handleSearch} />
     {/*
       quando vc usa 2 exclamacoes vc esta convertendo o resultado para boleano,
       caso vc atribua 0 o valor zero sera apresentado quando vc faz dessa forma não
@@ -34,9 +34,10 @@ const AppContent = ({ userinfo, repos, starred }) => (
 )
 
 AppContent.propTypes = {
-  userinfo: PropTypes.object.isRequired,
+  userinfo: PropTypes.object,
   repos: PropTypes.array.isRequired,
-  starred: PropTypes.array.isRequired
+  starred: PropTypes.array.isRequired,
+  handleSearch: PropTypes.func.isRequired
 }
 
 export default AppContent
