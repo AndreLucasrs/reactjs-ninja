d8899f3dc4848dbe03133ffe9e6f3ab5b7564813
André Lucas <andrelrs.v@outlook.com>
2020-05-28 15:28:21 -0300

    M2#A47 - Storybook

--- m02/working_with_assets/github-app/src/components/actions/actions.story.js
+++ m02/working_with_assets/github-app/src/components/actions/actions.story.js
@@ -4,15 +4,8 @@ import { storiesOf, action } from '@kadira/storybook'
 import React from 'react'
 import Actions from './index'
 
-const stories = storiesOf('Div', module)
-stories.add('first story', () => (
-  <Actions
-    getRepos={action('Get Repos')}
-    getStarred={action('Get Starred')}
-  />
-))
-
-stories.add('second story', () => (
+const stories = storiesOf('Actions', module)
+stories.add('Actions component', () => (
   <Actions
     getRepos={action('Get Repos')}
     getStarred={action('Get Starred')}
