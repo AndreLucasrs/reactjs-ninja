7dea9acec0d8064a2d106f6dd1b0e2ba724c4296
André Lucas <andrelrs.v@outlook.com>
2020-06-02 16:25:15 -0300

    M2#A60 - Paginação - storybook - parte 3

--- m02/applications/github-app/src/components/pagination/pagination.story.js
+++ m02/applications/github-app/src/components/pagination/pagination.story.js
@@ -7,8 +7,31 @@ import Pagination from './index'
 const stories = storiesOf('<Pagination />', module)
 
 stories.add('without props', () => (
+  <Pagination />
+))
+
+stories.add('with total and activePages', () => (
   <Pagination
     total={10}
-    pageLink={'http://mypage.com/page/%page%'}
+    activePage={5}
   />
+))
+
+stories.add('with page link', () => (
+  <Pagination
+    total={3}
+    activePage={1}
+    pageLink={'http://mypage.com/page/%page%'}
+    />
+))
+
+stories.add('with callback', () => (
+  <Pagination
+    total={15}
+    activePage={7}
+    pageLink={'http://mypage.com/page/%page%'}
+    onClick={(page) => {
+      window.alert(page)
+    }}
+    />
 ))
