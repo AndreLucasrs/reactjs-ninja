1bc87d192812a929ee6b685b1b0c177856d6b83a
André Lucas <andrelrs.v@outlook.com>
2020-06-03 20:56:26 -0300

    M2#A63 - Abstraindo configuração padrão do webpack - parte 2

---
+++ m02/applications/github-app/webpack/prod.config.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const webpack = require('webpack')
+const validate = require('webpack-validator')
+
+const common = require('./common')
+
+const HtmlPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = validate({
+
+  entry: common.entry,
+  output: common.output,
+
+  plugins: [
+    new ExtractTextPlugin('[name]-[hash].css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new HtmlPlugin(common.htmlPluginConfig('template.html'))
+  ],
+
+  module: {
+    preLoaders: [common.sandardPreLoaders],
+
+    loaders: [
+      common.jsLoader, 
+      Object.assign({}, common.cssLoader, {
+        loaders: undefined,
+        loader: ExtractTextPlugin.extract(common.cssLoader.loaders[0], common.cssLoader.loaders[1])
+      })
+    ]
+  },
+
+  resolve: common.resolve
+})
