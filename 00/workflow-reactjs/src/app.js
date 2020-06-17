d0f35b2687fed77d5dc155cf69950d53f05b00e2
André Lucas <andrelrs.v@outlook.com>
2020-06-17 19:00:21 -0300

    update:   projeto e webpack

--- 00/workflow-reactjs/src/app.js
+++ 00/workflow-reactjs/src/app.js
@@ -1,11 +1,38 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
-  return (
-    <div>My App</div>
-  )
+import './css/style.css'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = { counter: 0 }
+  }
+
+  incrementCounter (state) {
+    return { counter: state.counter + 1 }
+  }
+
+  incrementCounter2 (state) {
+    return { counter: state.counter + 2 }
+  }
+
+  componentDidMount () {
+    this.setState(this.incrementCounter)
+    this.setState(this.incrementCounter)
+    this.setState(this.incrementCounter)
+    this.setState(this.incrementCounter2)
+  }
+
+  render () {
+    const { counter } = this.state
+    console.log('render:', counter)
+
+    return (
+      <div>counter: {counter}</div>
+    )
+  }
 }
 
 export default App
