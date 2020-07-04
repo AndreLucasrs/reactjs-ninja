5753af34047e40544ffc61369dcfb184f4dba88e
André Lucas <andrelrs.v@outlook.com>
2020-07-03 21:56:58 -0300

    171. M2#A105 - Markdown - reduzindo o bundle de produção

--- m02/applications/markdown-editor/webpack/prod.config.js
+++ m02/applications/markdown-editor/webpack/prod.config.js
@@ -32,23 +32,25 @@ module.exports = {
       name: 'react-build',
       chunks: ['main'],
       minChunks: ({ resource }) => (
-        /node_modules\/(react(-dom)?|fbjs)\//.test(resource)
+        /node_modules\/(react(-dom)?|fbjs)\//.test(resource) ||
+        /node_modules\/preact/.test(resource)
       )
     }),
 
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       chunks: ['main'],
-      minChunks: ({ resource }) => {
+      minChunks: ({ resource }) => (
         /node_modules/.test(resource)
-      }
+      )
     }),
 
     new HtmlPlugin(Object.assign({}, common.htmlPluginConfig, {
-      minify: { CollapseWhitespace: true },
+      minify: { collapseWhitespace: true },
+
       chunksSortMode: (chunk1, chunk2) => {
         const order = ['react-build', 'vendor', 'main']
-        const left = oder.indexOf(chunk1.names[0])
+        const left = order.indexOf(chunk1.names[0])
         const right = order.indexOf(chunk2.names[0])
         return left - right
       }
@@ -76,5 +78,10 @@ module.exports = {
     ]
   },
 
-  resolve: common.resolve
+  resolve: {
+    alias: Object.assign({}, common.resolve.alias, {
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    })
+  }
 }
