a355b306e8e33b5610494c20f872dc442f68bdbf
André Lucas <andrelrs.v@outlook.com>
2020-07-08 16:00:42 -0300

    M3#A13 - "Contador" no React - estado no Redux

--- m03/counter-react/src/components/counter/index.js
+++ m03/counter-react/src/components/counter/index.js
@@ -6,19 +6,27 @@ import Counter from './counter'
 class CounterContainer extends PureComponent {
   constructor () {
     super()
-    this.state = {
-      counter: 0
-    }
 
-    this.increment = () => this.setState({ counter: this.state.counter + 1 })
-    this.decrement = () => this.setState({ counter: this.state.counter - 1 })
+    this.increment = () => {
+      this.props.store.dispatch({ type: 'INCREMENT' })
+    }
+    this.decrement = () => {
+      this.props.store.dispatch({ type: 'DECREMENT' })
+    }
+  }
+
+  componentDidMount () {
+    this.unsubscribe = this.props.store.subscribe(() => this.forceUpdate())
+  }
+
+  componentWillUnmount () {
+    this.unsubscribe()
   }
 
   render () {
-    const { counter } = this.state
     return (
       <Counter
-        counter={counter}
+        counter={this.props.store.getState()}
         decrement={this.decrement}
         increment={this.increment}
       />
