a924b7e6b341fa52c70b59dd4cf8f2a62c74a519
André Lucas <andrelrs.v@outlook.com>
2020-07-10 19:00:46 -0300

    M3#A45 - Todo List - Atualizar estado do filtro

--- m03/todo-list/src/components/filter/index.js
+++ m03/todo-list/src/components/filter/index.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import * as actions from 'reducers/visibility-filter/actions'
+import { setVisibilityFilter } from 'reducers/visibility-filter/action-creators'
 
 const filterItems = [
     { label: 'Todos', action: actions.SHOW_ALL },
@@ -10,20 +11,43 @@ const filterItems = [
     { label: 'A Fazer', action: actions.SHOW_ACTIVE }
 ]
 
-const Filter = ({ activeFilter }) => (
+const Filter = ({ activeFilter, handleFilter }) => (
   <div>
     <h3>Mostrar</h3>
     {filterItems.map((item) => {
       if (item.action === activeFilter) {
-        return <span style={{ marginRight: 10 }} key={item.action}>{item.label}</span>
+        return (
+          <span
+            style={{ marginRight: 10 }}
+            key={item.action}
+            >
+            {item.label}
+          </span>
+        )
       }
-      return <a style={{ marginRight: 10 }} href='' key={item.action}>{item.label}</a>
+      return (
+        <a
+          style={{ marginRight: 10 }}
+          href=''
+          key={item.action}
+          onClick={handleFilter(item.action)}
+            >
+          {item.label}
+        </a>
+      )
     })}
   </div>
 )
 
-const mapsStateToProps = (state) => ({
+const mapStateToProps = (state) => ({
   activeFilter: state.visibilityFilter
 })
 
-export default connect(mapsStateToProps)(Filter)
+const mapDispatchToProps = (dispatch) => ({
+  handleFilter: (filter) => (e) => {
+    e.preventDefault()
+    dispatch(setVisibilityFilter(filter))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter)
