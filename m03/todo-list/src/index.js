39a61e1ba2f809bd467f27c36acc7336efe7bfaf
André Lucas <andrelrs.v@outlook.com>
2020-07-13 18:45:45 -0300

    M3#A62 - replaceReducer - hot reload para reducers

--- m03/todo-list/src/index.js
+++ m03/todo-list/src/index.js
@@ -8,29 +8,13 @@ import { Provider } from 'react-redux'
 import App from './app'
 import reducer from 'reducers'
 
-const initialState = {
-  todos: [{
-    text: 'auto',
-    id: '123',
-    completed: false
-  }],
-
-  address: {
-    address: 'Rua tal',
-    city: 'Cidade tal',
-    code: '0101010',
-    district: 'Bairro tal',
-    state: 'ET',
-    status: 200
-  }
-}
-
-const store = createStore(reducer, initialState)
+const store = createStore(reducer)
 
 const renderState = () => {
   console.log('State ', store.getState())
 }
 store.subscribe(renderState)
+renderState()
 
 const renderApp = (NextApp) => {
   render(
@@ -49,5 +33,9 @@ if (module.hot) {
   module.hot.accept('./app', () => {
     const NextApp = require('./app').default
     renderApp(NextApp)
+  })
+  module.hot.accept('reducers', () => {
+    const nextReducer = require('reducers').default
+    store.replaceReducer(nextReducer)
   })
 }
