6fd1d01eedf729cb75f7c06f6b05281e77a23cb4
André Lucas <andrelrs.v@outlook.com>
2020-07-14 18:25:47 -0300

    M3#A72 - Reactflix - Estrutura base da aplicação

--- m03/reactflix/src/app.js
+++ m03/reactflix/src/app.js
@@ -1,12 +1,49 @@
 'use strict'
 
 import React from 'react'
+import styled, { injectGlobal } from 'styled-components'
 
 import 'normalize.css'
 import 'milligram'
 
 const App = () => (
-  <h1>Olá</h1>
+  <Container>
+    <Header>
+      <h1>Reactflix</h1>
+    </Header>
+    <Main>
+      Conteúdo
+    </Main>
+    <Footer>
+      &copy; 2020
+    </Footer>
+  </Container>
 )
+
+injectGlobal`
+  html, body, div[data-js="app"] {
+    height: 100%;
+  }
+`
+
+const headerHeight = '60px'
+const footerHeight = '30px'
+
+const Container = styled.div`
+  height: 100%;
+`
+
+const Header = styled.header`
+  height: ${headerHeight};
+  background: #333;
+`
+const Main = styled.main`
+  min-height: calc(100% - ${headerHeight} - ${footerHeight});
+`
+
+const Footer = styled.footer`
+height: ${footerHeight};
+background: #333;
+`
 
 export default App
