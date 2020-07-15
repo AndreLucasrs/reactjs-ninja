75b45b3c75339edf9658d26314b04b15b3358c3d
André Lucas <andrelrs.v@outlook.com>
2020-07-15 17:31:53 -0300

    M3#A82 - Reactflix - Criar formulário de cadastro do vídeo

---
+++ m03/reactflix/src/components/register-video.js
@@ -0,0 +1,24 @@
+'use strict'
+
+import React from 'react'
+import styled from 'styled-components'
+
+const RegisterVideo = () => (
+  <Form>
+    <h2>Cadastrar Video</h2>
+
+    <label htmlFor='id'>ID do Video:</label>
+    <input type='text' id='id' name='id' />
+
+    <label htmlFor='title'>Titulo do Video:</label>
+    <input type='text' id='title' name='title' />
+
+    <button type='submit'>Cadastrar</button>
+  </Form>
+)
+
+const Form = styled.form`
+  padding: 10px
+`
+
+export default RegisterVideo
